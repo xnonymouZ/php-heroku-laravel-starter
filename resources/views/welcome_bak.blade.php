bfd621482d0c8c9f74792015be0b6bb14969250c
ChunKitChung <chung@arara.com>
2016-12-09 17:51:19 +0900

    fixing

--- resources/views/welcome_bak.blade.php
+++ resources/views/welcome_bak.blade.php
@@ -66,12 +66,14 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+        <!--
             @if (Route::has('login'))
                 <div class="top-right links">
                     <a href="{{ url('/login') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
                 </div>
             @endif
+        -->
 
             <div class="content">
                 <div class="title m-b-md">
