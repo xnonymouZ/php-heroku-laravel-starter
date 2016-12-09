030001c72bb497c39e45c6e40452aa2a174d1cf1
ChunKitChung <chung@arara.com>
2016-12-09 18:28:43 +0900

    another logout fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,7 @@ class LoginController extends Controller
      */
     //protected $redirectTo = '/home';
     protected $redirectTo = '/b';
+    protected $redirectAfterLogout = '/b';
 
     /**
      * Create a new controller instance.
@@ -36,5 +38,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->flush();
+        $request->session()->regenerate();
+        return redirect($this->redirectAfterLogout);
     }
 }
