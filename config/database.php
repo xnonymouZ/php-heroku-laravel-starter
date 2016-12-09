afb020200d5b26192ae64c4a0211c481cdc380b2
ChunKitChung <chung@arara.com>
2016-12-09 16:09:53 +0900

    db

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($host, $username, $password, $db);
 
 return [
 
